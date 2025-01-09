e947cfa9c05869ac6e101e43a84d6b4aae7137e1
vinipolitta <vinicius.politta1@gmail.com>
2025-01-09 16:07:33 -0300

    feat: Implementar os métodos para buscar e editar o cadastro na API

--- src/app/core/services/cadastro.service.ts
+++ src/app/core/services/cadastro.service.ts
@@ -1,18 +1,45 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { PessoaUsuaria } from '../types/type';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CadastroService {
-
   private apiUrl: string = environment.apiUrl;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   cadastrar(pessoaUsuaria: PessoaUsuaria): Observable<PessoaUsuaria> {
-    return this.http.post<PessoaUsuaria>(`${this.apiUrl}/auth/cadastro`, pessoaUsuaria);
+    return this.http.post<PessoaUsuaria>(
+      `${this.apiUrl}/auth/cadastro`,
+      pessoaUsuaria
+    );
+  }
+  buscarCadastro(token: string): Observable<PessoaUsuaria> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.get<PessoaUsuaria>(`${this.apiUrl}/auth/perfil`, {
+      headers,
+    });
+  }
+  editarCadastro(
+    pessoaUsuaria: PessoaUsuaria,
+    token: string
+  ): Observable<PessoaUsuaria> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.patch<PessoaUsuaria>(
+      `${this.apiUrl}/auth/perfil`,
+      pessoaUsuaria,
+      {
+        headers,
+      }
+    );
   }
 }
--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,21 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CadastroService } from 'src/app/core/services/cadastro.service';
+import { FormularioService } from 'src/app/core/services/formulario.service';
+import { TokenService } from 'src/app/core/services/token.service';
+import { UserService } from 'src/app/core/services/user.service';
+import { PessoaUsuaria } from 'src/app/core/types/type';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.scss'],
 })
-export class PerfilComponent {
+export class PerfilComponent implements OnInit {
   public perfilComponent = true;
-  public titulo = 'Ola, X';
+  public titulo = 'Ola ';
   public textoBtn = 'ATUALIZAR';
+  public token = '';
+  public cadastro!: PessoaUsuaria;
+  public nome: string = '';
+  public form!: FormGroup<any> | null;
 
+  constructor(
+    private tokenService: TokenService,
+    private cadastroService: CadastroService,
+    private formularioService: FormularioService,
+    private userService: UserService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.token = this.tokenService.retornarToken();
+    this.cadastroService.buscarCadastro(this.token).subscribe({
+      next: (resposta: PessoaUsuaria) => {
+        this.cadastro = resposta;
+        this.nome = resposta.nome;
+        this.carregarForm();
+      },
+      error: (error) => {
+        console.error('Erro ao buscar cadastro', error);
+      },
+    });
+  }
+
+  carregarForm() {
+    this.form = this.formularioService.getCadastro();
+    this.form?.patchValue({
+      nome: this.cadastro.nome,
+      nascimento: this.cadastro.nascimento,
+      cpf: this.cadastro.cpf,
+      telefone: this.cadastro.telefone,
+      email: this.cadastro.email,
+      senha: this.cadastro.senha,
+      genero: this.cadastro.genero,
+      cidade: this.cadastro.cidade,
+      estado: this.cadastro.estado,
+    });
+  }
   deslogar() {
-    console.log("Deslogar");
-
+    this.userService.logout();
+    this.router.navigate(['/login']);
   }
   atualizar() {
-    console.log("Atualizar");
+    const dadosAtualizados = {
+      nome: this.form?.value.nome,
+      nascimento: this.form?.value.nascimento,
+      cpf: this.form?.value.cpf,
+      telefone: this.form?.value.telefone,
+      email: this.form?.value.email,
+      senha: this.form?.value.senha,
+      genero: this.form?.value.genero,
+      cidade: this.form?.value.cidade,
+      estado: this.form?.value.estado,
+    };
 
+    this.cadastroService
+      .editarCadastro(dadosAtualizados, this.token)
+      .subscribe({
+        next: () => {
+          alert('Cadastro atualizado com sucesso!');
+          this.router.navigate(['/']);
+        },
+        error: (error) => {
+          console.error('Erro ao atualizar cadastro', error);
+        },
+      });
   }
 }
